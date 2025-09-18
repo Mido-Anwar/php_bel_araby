2e23da13ae7bf037bcfc715fd1f174ce511ce513
Mido Anwar <mimianwar33@hotmail.com>
2025-09-18 15:58:17 +0300

    styly tech dashboard

--- resources/views/learning_and_references/Technology/index.blade.php
+++ resources/views/learning_and_references/Technology/index.blade.php
@@ -9,27 +9,29 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <div class="my-2 p-3">
-                        <a href="{{ route('tech.create') }}" class="px-4 py-2 bg-blue-600 text-white rounded-md ">
+                    <div class="my-4 p-5">
+                        <a href="{{ route('tech.create') }}" class="btn-create">
                             + Create Technology
                         </a>
                     </div>
-                    <div class="m-2 p-3 flex flex-col w-1/2">
-                        @foreach ($techs as $tech)
-                            <a href="{{ route('tech.show', $tech->name) }}"
-                                class="my-2 px-4 py-2 w-1/2 bg-black text-white rounded-md ">{{ $tech->name }}</a>
+
+                    @foreach ($techs as $tech)
+                        <div class="btn-container">
+                            <a href="{{ route('tech.show', $tech->name) }}" class="btn-show">
+                                {{ $tech->name }}
+                            </a>
 
                             <form action="{{ route('tech.destroy', $tech->name) }}" method="POST"
                                 onsubmit="return confirm('Are you sure you want to delete this technology?');">
                                 @csrf
                                 @method('DELETE')
-                                <button type="submit" class="px-3 py-2 bg-red-600 text-white rounded hover:bg-red-700">
-                                    Delete {{ $tech->name }}
+                                <button type="submit" class="btn-delete">
+                                    X
                                 </button>
                             </form>
-                            <hr class="my-2">
-                        @endforeach
-                    </div>
+                        </div>
+                    @endforeach
+
                 </div>
             </div>
         </div>
