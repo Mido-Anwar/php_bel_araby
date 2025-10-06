2dda6abb4c2ab82dab4950cae135e420478ff184
Mido Anwar <mimianwar33@hotmail.com>
2025-10-06 11:54:25 +0300

    role permissions  add roles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,19 +13,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-         User::factory(10)->create();
+    //     User::factory(10)->create();
 
+    $this->call(RolesAndPermissionsSeeder::class);
         // User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
        // $this->call(PostSeeder::class);
 
-               $this->call([
-            TechnologySeeder::class,
-            SectionSeeder::class,
-            ConceptSeeder::class,
-            BuiltInFunctionSeeder::class,
-        ]);
+        //        $this->call([
+        //     TechnologySeeder::class,
+        //     SectionSeeder::class,
+        //     ConceptSeeder::class,
+        //     BuiltInFunctionSeeder::class,
+        // ]);
     }
 }
