577dcb21ea33e66b60be1962cadcdc46f6dabc70
Mido Anwar <mimianwar33@hotmail.com>
2025-10-11 13:39:39 +0300

    refactor crud2

--- resources/views/blog/post/index.blade.php
+++ resources/views/blog/post/index.blade.php
@@ -9,74 +9,69 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <div class="w-full overflow-x-auto">
-                        <table class="w-full border border-gray-200 dark:border-gray-700 text-sm">
-                            <thead class="bg-gray-100 dark:bg-gray-800">
-                                <tr>
-                                    <th class="px-4 py-2 text-left font-medium text-gray-700 dark:text-gray-300">ID</th>
-                                    <th class="px-4 py-2 text-left font-medium text-gray-700 dark:text-gray-300">Title
-                                    </th>
-                                    <th class="px-4 py-2 text-left font-medium text-gray-700 dark:text-gray-300">Actions
-                                    </th>
-                                </tr>
-                            </thead>
-                            <tbody class="divide-y divide-gray-200 dark:divide-gray-700">
-                                <tr class="hover:bg-gray-50 dark:hover:bg-gray-700">
-                                    @foreach ($posts as $post)
-                                        <td class="px-4 py-4">{{ $post->id }}</td>
-                                        <td class="px-4 py-4">{{ $post->title }}</td>
-                                        <td class="px-4 py-4 border space-x-4">
-                                            <a href="{{ route('post.edit', $post->id) }}" class="btn-edit">Edit</a>
-                                            <a href="{{ route('post.destroy', $post->id) }}"
-                                                class="btn-delete">Delete</a>
-                                        </td>
-                                    @endforeach
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                    <div class="my-3 bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+        <x-dashboard-container>
+            <div class="w-full overflow-x-auto">
+                <table class="w-full border border-gray-200 dark:border-gray-700 text-sm">
+                    <thead class="bg-gray-100 dark:bg-gray-800">
+                        <tr>
+                            <th class="px-4 py-2 text-left font-medium text-gray-700 dark:text-gray-300">ID</th>
+                            <th class="px-4 py-2 text-left font-medium text-gray-700 dark:text-gray-300">Title
+                            </th>
+                            <th class="px-4 py-2 text-left font-medium text-gray-700 dark:text-gray-300">Actions
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody class="divide-y divide-gray-200 dark:divide-gray-700">
+                        @foreach ($posts as $post)
+                            <tr class="hover:bg-gray-50 dark:hover:bg-gray-700">
 
-                        <div class="p-6 my-2 text-gray-900 dark:text-gray-100">
-                            <x-hidden-form method="POST" btnName="add post" action-url="{{ route('post.store') }}"
-                                :open="false" :fields="[
-                                    [
-                                        'name' => 'user_id',
-                                        'type' => 'hidden',
-                                        'label' => '',
-                                        'placeholder' => 'Enter Post title',
-                                        'value' => auth()->user()->id,
-                                    ],
-                                    [
-                                        'name' => 'title',
-                                        'type' => 'text',
-                                        'label' => 'Post Title',
-                                        'placeholder' => 'Enter Post title',
-                                    ],
-                                    [
-                                        'name' => 'body',
-                                        'type' => 'textarea',
-                                        'label' => 'Post Body',
-                                        'placeholder' => 'Write an Post...',
-                                        'rows' => 5,
-                                    ],
-                                ]">
+                                <td class="px-4 py-4">{{ $post->id }}</td>
+                                <td class="px-4 py-4">{{ $post->title }}</td>
+                                <td class="px-4 py-4 border space-x-4">
+                                    <a href="{{ route('post.edit', $post->id) }}" class="btn-edit">Edit</a>
+                                    <a href="{{ route('post.destroy', $post->id) }}" class="btn-delete">Delete</a>
+                                </td>
+                            </tr>
+                        @endforeach
 
-
-                            </x-hidden-form>
-                                 {{-- Success Message --}}
-                    @if (session('success-stored-post'))
-                        <div class="mt-6 p-4 bg-green-100 text-green-700 rounded-lg border border-green-300">
-                            {{ session('success-stored-post') }}
-                        </div>
-                    @endif
-                        </div>
-                    </div>
-                </div>
+                    </tbody>
+                </table>
             </div>
-        </div>
+
+        </x-dashboard-container>
+        <x-dashboard-container>
+            <x-hidden-form method="POST" btnName="add post" action-url="{{ route('post.store') }}" :open="false"
+                :fields="[
+                    [
+                        'name' => 'user_id',
+                        'type' => 'hidden',
+                        'label' => '',
+                        'placeholder' => 'Enter Post title',
+                        'value' => auth()->user()->id,
+                    ],
+                    [
+                        'name' => 'title',
+                        'type' => 'text',
+                        'label' => 'Post Title',
+                        'placeholder' => 'Enter Post title',
+                    ],
+                    [
+                        'name' => 'body',
+                        'type' => 'textarea',
+                        'label' => 'Post Body',
+                        'placeholder' => 'Write an Post...',
+                        'rows' => 5,
+                    ],
+                ]">
+
+
+            </x-hidden-form>
+            {{-- Success Message --}}
+            @if (session('success-stored-post'))
+                <div class="mt-6 p-4 bg-green-100 text-green-700 rounded-lg border border-green-300">
+                    {{ session('success-stored-post') }}
+                </div>
+            @endif
+        </x-dashboard-container>
     </div>
 </x-app-layout>
--- resources/views/blog/post/post-edit.blade.php
+++ resources/views/blog/post/post-edit.blade.php
@@ -3,54 +3,53 @@
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Show Post') }}
         </h2>
+        <p class="mt-2 text-sm text-gray-500 max-w-2xl">
+            Detailed view of the blog post and edit .
+        </p>
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    <h1 class="text-3xl font-bold mb-4">{{ $post->title }}</h1>
-                    <p class="mb-4">{{ $post->body }}</p>
-                    <a href="{{ route('posts.index', $post->id) }}" class="text-blue-500 hover:underline">Back to Posts</a>
-                </div>
-            </div>
-            <div class="my-3 bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
+        <x-dashboard-container>
+            <h1 class="text-3xl font-bold mb-4">{{ $post->title }}</h1>
+            <p class="mb-4">{{ $post->body }}</p>
+            <a href="{{ route('posts.index', $post->id) }}" class="text-blue-500 hover:underline">Back to
+                Posts</a>
+        </x-dashboard-container>
+        <x-dashboard-container>
 
-                    <x-hidden-form method="POST" btnName="edit post" action-url="{{ route('post.update', $post->id) }}"
-                        :open="false" :fields="[
-                            [
-                                'name' => 'user_id',
-                                'type' => 'hidden',
-                                'label' => '',
-                                'placeholder' => 'Enter Post title',
-                                'value' => auth()->user()->id,
-                            ],
-                            [
-                                'name' => 'title',
-                                'type' => 'text',
-                                'label' => 'Post Title',
-                                'placeholder' => 'Enter Post title',
-                                'value' => $post->title,
-                            ],
-                            [
-                                'name' => 'body',
-                                'type' => 'textarea',
-                                'label' => 'Post Body',
-                                'placeholder' => 'Write an Post...',
-                                'rows' => 5,
-                                'value' => $post->body,
-                            ],
-                        ]">
-                    </x-hidden-form>
-                    {{-- Success Message --}}
-                    @if (session('success-updated-post'))
-                        <div class="mt-6 p-4 bg-green-100 text-green-700 rounded-lg border border-green-300">
-                            {{ session('success-updated-post') }}
-                        </div>
-                    @endif
+            <x-hidden-form method="POST" btnName="edit post" action-url="{{ route('post.update', $post->id) }}"
+                :open="false" :fields="[
+                    [
+                        'name' => 'user_id',
+                        'type' => 'hidden',
+                        'label' => '',
+                        'placeholder' => 'Enter Post title',
+                        'value' => auth()->user()->id,
+                    ],
+                    [
+                        'name' => 'title',
+                        'type' => 'text',
+                        'label' => 'Post Title',
+                        'placeholder' => 'Enter Post title',
+                        'value' => $post->title,
+                    ],
+                    [
+                        'name' => 'body',
+                        'type' => 'textarea',
+                        'label' => 'Post Body',
+                        'placeholder' => 'Write an Post...',
+                        'rows' => 5,
+                        'value' => $post->body,
+                    ],
+                ]">
+            </x-hidden-form>
+            {{-- Success Message --}}
+            @if (session('success-updated-post'))
+                <div class="mt-6 p-4 bg-green-100 text-green-700 rounded-lg border border-green-300">
+                    {{ session('success-updated-post') }}
                 </div>
-            </div>
-        </div>
+            @endif
+        </x-dashboard-container>
+
     </div>
 </x-app-layout>
