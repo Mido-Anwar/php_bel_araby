2e37fe4ad9ea4cfd23ecf8a3420142fcf01ca02b
Mido Anwar <mimianwar33@hotmail.com>
2025-10-13 02:55:38 +0300

    make new component

--- app/Helpers/TextLanguageHelper.php
+++ app/Helpers/TextLanguageHelper.php
@@ -1,13 +1,13 @@
 <?php
 
 if (!function_exists('detectLanguage')) {
-    function detectLanguage($text)
+    function textDir($text)
     {
         $arabicCount = preg_match_all('/[\p{Arabic}]/u', $text);
         $englishCount = preg_match_all('/[A-Za-z]/', $text);
 
-        if ($arabicCount > $englishCount) return 'ar';
-        if ($englishCount > $arabicCount) return 'en';
+        if ($arabicCount > $englishCount) return 'rtl';
+        if ($englishCount > $arabicCount) return 'ltr';
         return 'unknown';
     }
 }
---
+++ resources/views/components/dashboard-pargraph.blade.php
@@ -0,0 +1,8 @@
+ <p dir="{{ textDir($text) }}"
+   class="text-lg my-7 leading-relaxed text-gray-700 dark:text-gray-300
+          bg-gray-50 dark:bg-gray-800
+          p-4 rounded-2xl shadow-sm
+          border border-gray-200 dark:border-gray-700
+          transition-all duration-300 hover:shadow-md">
+    {{ $text }}
+</p>
