ac7c2105d32bf4cfd1444c1abd1a9fd40e86b4e1
Mido Anwar <mimianwar33@hotmail.com>
2025-10-27 13:32:47 +0300

    mido

--- resources/views/learning_and_references/section/showConceptOrBuiltinFun.blade.php
+++ resources/views/learning_and_references/section/showConceptOrBuiltinFun.blade.php
@@ -1,16 +1,24 @@
 <x-app-layout>
     <x-slot name="header">
-        <x-dashboard-head :text="$concept->name" />
+        @if (route('concept.show', $concept->id))
+            <x-dashboard-head :text="$concept->name" />
+        @elseif (route('builtin.show', $builtinFunction->id))
+            <x-dashboard-head :text="$builtinFunction->name" />
+        @endif
 
     </x-slot>
 
     <div class="py-12">
         <x-dashboard-container>
             {{-- Section Card --}}
+            @if (route('concept.show', $concept->id))
+                <x-dashboard-head :text="$concept->name" />
+                <x-dashboard-paragraph :text="$concept->syntax" />
+            @elseif (route('builtin.show', $builtinFunction->id))
+                <x-dashboard-head :text="$builtinFunction->name" />
+                <x-dashboard-paragraph :text="$builtinFunction->syntax" />
+            @endif
 
-            <x-dashboard-head :text="$concept->name" />
-
-            <x-dashboard-paragraph :text="$concept->syntax" />
         </x-dashboard-container>
     </div>
 </x-app-layout>
