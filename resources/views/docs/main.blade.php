304f2e2c3ecf19d4e2df010a490ec0e22bf376e7
Mido Anwar <mimianwar33@hotmail.com>
2025-12-29 02:23:10 +0200

    docs show work continue js

--- resources/views/docs/main.blade.php
+++ resources/views/docs/main.blade.php
@@ -1,5 +1,21 @@
 <x-master-layout>
 
-    <x-master.doc-view :technology="$technology" />
+    <div class="p-2 bg-slate-300 min-h-screen">
 
+        @foreach ($technology->sections as $section)
+            <div x-data="{ open: false }" class="border">
+
+                <button @click="open = ! open">{{ $section->title }}</button>
+                <div x-show="open">
+                    @foreach ($section->concepts as $concept)
+                        <button class=" flex p-2 border bg-red-200">{{ $concept->name }}</button>
+                    @endforeach
+                </div>
+            </div>
+        @endforeach
+
+
+        {{-- <x-master.doc-view :technology="$technology" /> --}}
+
+    </div>
 </x-master-layout>
