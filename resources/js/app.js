71cd7cfd3f0cfce9411a1b7b7706377311da5901
Mido Anwar <mimianwar33@hotmail.com>
2026-01-01 13:33:31 +0200

    fix js function for doc view page in shaa allah will be good

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,20 +7,16 @@ window.Alpine = Alpine;
 Alpine.start();
 
 
-document.addEventListener('focusin', (e) => {
-  if (e.target && e.target.id === 'deleteBtn') {
-    alert("Are you sure you want to delete this user?");
-  }
-});
-
-document.addEventListener('click', function (e) {
-    const btn = e.target.closest('.section-btn');
+// Event delegation for section buttons
+document.addEventListener("click", function (e) {
+    // Check if the clicked element is a section button
+    const btn = e.target.closest(".section-btn");
     if (!btn) return;
-
+    // Toggle the visibility of the section body
     const targetId = btn.dataset.target;
     const body = document.getElementById(targetId);
 
     if (body) {
-        body.classList.toggle('show');
+        body.classList.toggle("show");
     }
 });
